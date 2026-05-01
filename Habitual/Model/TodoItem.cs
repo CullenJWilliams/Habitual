fd394ec2f64f614942ed74b67aae5cbeba35decd
Cullen Williams <cullenjwilliams@protonmail.com>
2026-05-01 14:40:51 -0600

    cleanup

---
+++ Habitual/Model/TodoItem.cs
@@ -0,0 +1,20 @@
+public class TodoItem
+{
+    public string? Title { get; set; }
+    public bool Completed
+    {
+        get
+        {
+            return CompletedOn <= DateTime.Now;
+        }
+        set
+        {
+            if (CompletedOn is null)
+                CompletedOn = DateTime.Now;
+            else
+                CompletedOn = null;
+        }
+    }
+
+    public DateTime? CompletedOn { get; set; }
+}
--- Habitual/Model/TodoList.cs
+++ Habitual/Model/TodoList.cs
@@ -1,5 +1,7 @@
-public class TodoItem
+public class TodoList
 {
+
+    public List<TodoItem> Items {get; set;} = [];
     public string? Title {get;set;}
     public bool Completed {get;set;}
 }
